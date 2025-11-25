45fb3568ee6842d8069d0a4c7718fc2949e5b282
Saloni Jain <saloni.jain@talview.com>
2025-11-25 06:51:23 +0530

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
+const repo = process.env.NODE_ENV === 'production' ? '/expense-tracker' : '';
+
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/expense-tracker' : '',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-    ],
-  },
+    output: 'export',
+    trailingSlash: true,
+    basePath: repo,
+    assetPrefix: repo ? `${repo}/` : undefined,
+    images: {
+        unoptimized: true,
+        remotePatterns: [
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+        ],
+    },
 };
 
 export default nextConfig;
